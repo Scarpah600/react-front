1adaa1c52811412eaa47a4e774ca4e600fd20060
Scarpah600 <gustavo.scarpin.2020@gmail.com>
2020-10-10 15:44:43 -0300

    atualizado webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,46 @@
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-
 module.exports = {
     entry: './src/index.jsx',
     output: {
         path: __dirname + '/public',
-        filename:'./app.js'
+        filename: './app.js'
     },
-    devServer:{
-        port:8080,
+    devServer: {
+        port: 8080,
         contentBase: './public',
     },
     resolve: {
-        extensions: ['','.js','.jsx'],
-        alias:{
-            modules: _dirname +'/node_modules',
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
-            bootstrap: 'module/admin-lte/bootstrap/js/bootstrap.js'
+            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugins: [
+    plugins: [ 
         new webpack.ProvidePlugin({
             $: 'jquery',
-            jquery: 'jquery',
+            jQuery: 'jquery',
             'window.jQuery': 'jquery'
         }),
         new ExtractTextPlugin('app.css')
     ],
-    module:{
+    module: {
         loaders: [{
             test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
-            query:{
-                presets: ['es2015','react'],
-                plugins: ['transform-object-rest-spead']
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
             }
-        },{
+        }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        },{
-            teste: /\.woff|.worff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
             loader: 'file'
         }]
     }
