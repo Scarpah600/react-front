528d13cbff906bba258880c06318d38bce4ae6ae
Scarpah600 <gustavo.scarpin.2020@gmail.com>
2020-10-16 10:03:15 -0300

    atualizado front-end

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2020 <br></br>
+            Criador: Gustavo Scarpin <br></br>
+            E-mail: gustavo.scarpin.2020@gmail.com <br></br>
+            <img src="https://www.montanainstruments.com/images/social/footer/linkedin.png" alt="Footer" height="15" width="15">   
+                </img><a href="https://www.linkedin.com/in/gustavo-scarpin-21972b140/"  target='_blank'>Linkedin</a><br></br>
+        </strong>
+    </footer>
+)
